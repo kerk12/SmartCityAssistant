16f2d7876c73c5476bfb53456923f61fc5c1c503
Kyriakos Giannakis <kerk12gr@gmail.com>
2016-12-09 15:17:36 +0200

    Created a basic map structure.

--- app/src/main/java/com/kerk12/smartcityassistant/MainMenuFragment.java
+++ app/src/main/java/com/kerk12/smartcityassistant/MainMenuFragment.java
@@ -1,11 +1,13 @@
 package com.kerk12.smartcityassistant;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
@@ -15,11 +17,18 @@ import android.widget.Toast;
  */
 public class MainMenuFragment extends Fragment {
 
+    private OnClickListener myOnClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            //Toast.makeText(getActivity(), "Test", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(getActivity(), SmartSchedulePlanner.class);
+            startActivity(i);
+        }
+    } ;
 
     public MainMenuFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,12 +37,7 @@ public class MainMenuFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_main_menu, container, false);
         CardView MapChoice = (CardView) v.findViewById(R.id.MainMenuMapChoice);
 
-        MapChoice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "Test", Toast.LENGTH_SHORT).show();
-            }
-        });
+        MapChoice.setOnClickListener(myOnClickListener);
         return v;
     }
 
