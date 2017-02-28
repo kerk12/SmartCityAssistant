ab86078abb9d0bd982cef4195693b083317385e8
Kyriakos Giannakis <kerk12gr@gmail.com>
2017-02-28 16:45:52 +0200

    Added the about dialog.

--- app/src/main/java/com/kerk12/smartcityassistant/MainMenuActivity.java
+++ app/src/main/java/com/kerk12/smartcityassistant/MainMenuActivity.java
@@ -1,8 +1,15 @@
 package com.kerk12.smartcityassistant;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -13,5 +20,39 @@ public class MainMenuActivity extends AppCompatActivity {
 
         MainMenuFragment f = new MainMenuFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.frameContainerMainMenu, f, null).commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.about_choice:
+                AlertDialog.Builder bob = new AlertDialog.Builder(MainMenuActivity.this);
+                bob.setTitle(getResources().getString(R.string.about_the_app))
+                        .setMessage(getResources().getString(R.string.copyright))
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setNeutralButton("Προβολή κώδικα στο GitHub", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.github_link)));
+                                startActivity(i);
+                            }
+                        })
+                        .show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
